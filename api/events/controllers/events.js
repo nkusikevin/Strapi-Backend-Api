d6327d2539e316835981875fcacbfb5bb354ff27
“nkusikevin” <nkusikvnhart@gmail.com>
2021-11-05 23:29:25 +0200

    adding is owner policy

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -6,9 +6,21 @@ const { sanitizeEntity } = require("strapi-utils");
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
-
 module.exports = {};
 module.exports = {
+  // Create event with linked user
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      data.user = ctx.state.user.id;
+      entity = await strapi.services.events.create(data, { files });
+    } else {
+      ctx.request.body.user = ctx.state.user.id;
+      entity = await strapi.services.events.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.events });
+  },
   // Get logged in users
   async me(ctx) {
     const user = ctx.state.user;
